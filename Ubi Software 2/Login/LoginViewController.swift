b19d7952e14cd69e36a8f19c0d8eb2db876567ba
Jalil mackey <Jalil.Mackey@hgs.hiddengeniusproject.org>
2019-07-26 09:59:51 -0700

    first commit

---
+++ Ubi Software 2/Login/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  Ubi Software 2
+//
+//  Created by HGPMAC85 on 7/25/19.
+//  Copyright © 2019 HGPMAC85. All rights reserved.
+//
+
+import Foundation
---
+++ Ubi Software 2/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Ubi Software 2
+//
+//  Created by HGPMAC85 on 7/25/19.
+//  Copyright © 2019 HGPMAC85. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
